75b35c490ce70398823d7cc838ec0e7236db16ca
Fedor Metsger <fedor.metsger@gmail.com>
2023-03-19 12:49:08 +0300

    Add requirements.txt

--- src/views.py
+++ src/views.py
@@ -6,3 +6,7 @@ main_blueprint = Blueprint('main_blueprint', __name__)
 @main_blueprint.route('/')
 def page_index():
     return "Все работает"
+
+@main_blueprint.route('/frameworks')
+def page_index():
+    return "Frameworks"
