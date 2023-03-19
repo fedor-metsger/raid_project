347b3241d6a82c5f88a28a6d82e7d3bb791b4c9d
Fedor Metsger <fedor.metsger@gmail.com>
2023-03-19 12:43:39 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+
+from flask import Flask
+from app.views import main_blueprint
+
+app = Flask(__name__)
+app.register_blueprint(main_blueprint)
+app.register_blueprint(books_blueprint)
+
+if __name__ == "__main__":
+    app.run()
