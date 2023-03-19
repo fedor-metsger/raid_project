fe8e901e78b593090e36621163683b292b5f8942
Fedor Metsger <fedor.metsger@gmail.com>
2023-03-19 16:08:24 +0300

    Add class RaidSession

--- main.py
+++ main.py
@@ -1,27 +1,29 @@
 
 import os
 from flask import Flask
-from src.views import main_blueprint
-from src.raid_db import get_dsn, create_db as create_db, fill_db as fill_db
+import src.views as views
+from src.raid_db import RaidSession, get_dsn as get_dsn
 
 app = Flask(__name__)
-app.register_blueprint(main_blueprint)
+app.register_blueprint(views.main_blueprint)
 
 def main():
+#     dsn = get_dsn()
+#     if not dsn:
+#         print("""
+#     Необходимо задать параметры подключения к БД через переменные среды:
+#         PSTGRS_DB     - Имя базы данных
+#         PSTGRS_USER   - Имя пользователя
+#         PSTGRS_PASSWD - Пароль пользователя
+#         PSTGRS_HOSTNAME - Имя или адрес хоста
+# """)
+#         return
+#     rs = RaidSession(dsn)
+#
+#     if rs.create_db():
+#         rs.fill_db()
 
-    DSN = get_dsn()
-    if not DSN:
-        print("""
-    Необходимо задать параметры подключения к БД через переменные среды:
-        PSTGRS_DB     - Имя базы данных
-        PSTGRS_USER   - Имя пользователя
-        PSTGRS_PASSWD - Пароль пользователя
-        PSTGRS_HOSTNAME - Имя или адрес хоста
-    """)
-        return
-
-    create_db()
-    fill_db()
+    views.raid_session = None
 
     port_num = 10000
     if {"PORT"} <= os.environ.keys():
